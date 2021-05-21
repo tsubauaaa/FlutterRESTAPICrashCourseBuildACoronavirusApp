8538109ae5a084d46c5300350b58b7bc696cf021
tsubauaaa <tsubasa1173@gmail.com>
2021-05-21 15:48:49 +0900

    section2 done

---
+++ coronavirus_rest_api/android/app/src/main/kotlin/com/tsubauaaa/coronavirus_rest_api/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.tsubauaaa.coronavirus_rest_api
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ coronavirus_rest_api/lib/app/services/api_keys.dart
@@ -0,0 +1,4 @@
+class APIKey {
+  static String nconvSandboxKey =
+      '7029adaf76934aff087fc20541b5cff1fabad9181e525e733f413e5fb29c7eb8';
+}
