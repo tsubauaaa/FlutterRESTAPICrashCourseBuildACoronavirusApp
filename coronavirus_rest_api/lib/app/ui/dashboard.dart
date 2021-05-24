3aae8bedb988f71776dd2d57bc1b2b479f830f33
tsubauaaa <tsubasa1173@gmail.com>
2021-05-24 18:20:07 +0900

    section6 done

--- coronavirus_rest_api/lib/app/ui/dashboard.dart
+++ coronavirus_rest_api/lib/app/ui/dashboard.dart
@@ -54,7 +54,7 @@ class _DashBoardState extends State<DashBoard> {
   Widget build(BuildContext context) {
     final formatter = LastUpdatedDateFormatter(
         lastUpdated: _endpointsData != null
-            ? _endpointsData.values[Endpoint.cases].date
+            ? _endpointsData.values[Endpoint.cases]?.date
             : null);
     return Scaffold(
       appBar: AppBar(
@@ -71,7 +71,7 @@ class _DashBoardState extends State<DashBoard> {
               EndpointCard(
                 endpoint: endpoint,
                 value: _endpointsData != null
-                    ? _endpointsData.values[endpoint].value
+                    ? _endpointsData.values[endpoint]?.value
                     : null,
               ),
           ],
